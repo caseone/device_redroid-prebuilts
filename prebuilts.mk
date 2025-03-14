5dbb5e238ed9185539f6a2db89d8debcef0f2551
irvinli <421325991@qq.com>
2025-03-15 01:18:23 +0800

    init@20250315

---
+++ prebuilts.mk
@@ -0,0 +1,20 @@
+PRODUCT_PACKAGES += \
+    amdgpu.ids.redroid \
+    libEGL_mesa \
+    libGLESv1_CM_mesa \
+    libGLESv2_mesa \
+    vulkan.broadcom \
+    vulkan.freedreno \
+    vulkan.radeon \
+    vulkan.virtio \
+    vulkan.lvp \
+    vulkan.nouveau \
+    gralloc.cros \
+    gralloc.gbm \
+    uinputd \
+    vncserver \
+    hwcomposer.redroid \
+
+
+# vaapi
+PRODUCT_PACKAGES += avcenc h264encode hevcencode jpegenc vp8enc vp9enc vainfo
